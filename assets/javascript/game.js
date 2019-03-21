1fc61ea2270b41cf871f66e8365edf78517acd27
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-03-21 17:40:21 -0400

    wip

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,160 +1,94 @@
+// characters along with their name, health points, attack power, counter attack
+// power, and their relative path image source
+
 var charactersArray = [
-{
-    name: "Mario",
-    healthPoints: 120,
-    initialAttackPower: 8,
-    attackPower: 8,
-    counterAttackPower: 2,
-    isPlayerOne: false,
-    isPlayerTwo: false,
-    img: 'mario.png',
-}, 
-{
-    name: "Kirby",
-    healthPoints: 100,
-    initialAttackPower: 9,
-    attackPower: 9,
-    counterAttackPower: 5,
-    isPlayerOne: false,
-    isPlayerTwo: false,
-    img: 'kirby.png',
-},
-{
-    name: "Donkey Kong",
-    healthPoints: 150,
-    initialAttackPower: 2,	
-    attackPower: 2,
-    counterAttackPower: 20,
-    isPlayerOne: false,
-    isPlayerTwo: false,
-    img: 'donkeykong.png',
-{
-    name: "Link",
-    healthPoints: 150,
-    initialAttackPower: 2,	
-    attackPower: 2,
-    counterAttackPower: 20,
-    isPlayerOne: false,
-    isPlayerTwo: false,
-    img: 'link.png',
-},
-{
+    {name: "Mario", hp: 120, ap: 8, cap: 2, img: 'mario.png',}, 
+    {name: "Kirby", hp: 100, ap: 9, cap: 5, img: 'kirby.png',},
+    {name: "Donkey Kong", hp: 150, ap: 2, cap: 20, img: 'donkeykong.png',},
+    {name: "Link", hp: 150, ap: 2, cap: 20, img: 'link.png',},
+    {
     name: "Ness",
-    healthPoints: 150,
-    initialAttackPower: 2,	
-    attackPower: 2,
-    counterAttackPower: 20,
-    isPlayerOne: false,
-    isPlayerTwo: false,
+    hp: 150,
+    ap: 2,
+    cap: 20,
     img: 'ness.png',
 },
-{
+    {
     name: "Samus Aran",
-    healthPoints: 150,
-    initialAttackPower: 2,	
-    attackPower: 2,
-    counterAttackPower: 20,
-    isPlayerOne: false,
-    isPlayerTwo: false,
+    hp: 150,
+    ap: 2,
+    cap: 20,
     img: 'samusaran.png',
 },
-{
+    {
     name: "Fox McCloud",
-    healthPoints: 150,
-    initialAttackPower: 2,	
-    attackPower: 2,
-    counterAttackPower: 20,
-    isPlayerOne: false,
-    isPlayerTwo: false,
+    hp: 150,
+    ap: 2,
+    cap: 20,
     img: 'fox.png',
 },
-{
+    {
     name: "Zelda",
-    healthPoints: 150,
-    initialAttackPower: 2,	
-    attackPower: 2,
-    counterAttackPower: 20,
-    isPlayerOne: false,
-    isPlayerTwo: false,
+    hp: 150,
+    ap: 2,
+    cap: 20,
     img: 'zelda.png',
 },
-{
+    {
     name: "Pikachu",
-    healthPoints: 150,
-    initialAttackPower: 2,	
-    attackPower: 2,
-    counterAttackPower: 20,
-    isPlayerOne: false,
-    isPlayerTwo: false,
+    hp: 150,
+    ap: 2,
+    cap: 20,
     img: 'pikachu.png',
 },
-{
+    {
     name: "Princess Peach",
-    healthPoints: 150,
-    initialAttackPower: 2,	
-    attackPower: 2,
-    counterAttackPower: 20,
-    isPlayerOne: false,
-    isPlayerTwo: false,
+    hp: 150,
+    ap: 2,
+    cap: 20,
     img: 'peach.png',
 },
-{
+    {
     name: "King Dedede",
-    healthPoints: 150,
-    initialAttackPower: 2,	
-    attackPower: 2,
-    counterAttackPower: 20,
-    isPlayerOne: false,
-    isPlayerTwo: false,
+    hp: 150,
+    ap: 2,
+    cap: 20,
     img: 'kingdedede.png',
 },
 {
     name: "Captain Falcon",
-    healthPoints: 150,
-    initialAttackPower: 2,	
-    attackPower: 2,
-    counterAttackPower: 20,
-    isPlayerOne: false,
-    isPlayerTwo: false,
+    hp: 150,
+    ap: 2,
+    cap: 20,
     img: 'falcon.png',
 },
 {
     name: "Jigglypuff",
-    healthPoints: 150,
-    initialAttackPower: 2,	
-    attackPower: 2,
-    counterAttackPower: 20,
-    isPlayerOne: false,
-    isPlayerTwo: false,
+    hp: 150,
+    ap: 2,
+    cap: 20,
     img: 'jigglypuff.png',
 },
 {
     name: "Luigi",
-    healthPoints: 150,
-    initialAttackPower: 2,	
-    attackPower: 2,
-    counterAttackPower: 20,
-    isPlayerOne: false,
-    isPlayerTwo: false,
+    hp: 150,
+    ap: 2,
+    cap: 20,
     img: 'luigi.png',
 },
 {
     name: "Yoshi",
-    healthPoints: 180,
-    initialAttackPower: 1,
-    attackPower: 1,
-    counterAttackPower: 25,
-    isPlayerOne: false,
-    isPlayerTwo: false,
+    hp: 180,
+    ap: 1,
+    cap: 25,
     img: 'yoshi.png',
 }];
 
 var player;
-var opponent;
-playerOneSelected = false;
-playerTwoSelected = false;
-gameStart = false;
-var enemiesRemaining = charactersArray.length - 1;
+var cpu;
+
+
+$(document).ready(function () {
 
 function characterDisplay() {
     $.each(charactersArray, function (index, value) {
@@ -165,7 +99,7 @@ function characterDisplay() {
             .attr('src', 'assets/images/' + charactersArray[index].img);
 
         var $characterHP = $('<p>').addClass('hp text-center')
-            .html('Health: ' + charactersArray[index].healthPoints);
+            .html('Health: ' + charactersArray[index].hp);
 
         var $newCharacter = $('<div>').addClass('character')
             .attr('data-id', index)
@@ -179,7 +113,17 @@ function characterDisplay() {
     })
 };
 
-function newPlayerSelection(event) {}
+function newPlayerSelection(event) {
+    player = charactersArray[$(this).attr('data-id')];
+
+    $(this).appendTo('#player-character')
+        .addClass('player');
+
+    $('#characters-select').children()
+        .appendTo('#enemy-select')
+        .addClass('enemy')
+        .one('click', newEnemySelected);
+
 
 // function preGame() {
 //     if (gameStart = false) {
@@ -211,3 +155,8 @@ function playerSelect(){
 
 
 playerSelect();
+
+
+
+// Ends $(document).ready()
+});
